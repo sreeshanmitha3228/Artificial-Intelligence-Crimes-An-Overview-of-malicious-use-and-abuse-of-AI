81c53f2a6a4b86f53dea96fe4514866a22fe44ad
sreeshanmitha3228 <131433164+sreeshanmitha3228@users.noreply.github.com>
2024-03-24 23:19:57 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,41 @@
+"""artificial_intelligence_crime URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from Remote_User import views as remoteuser
+from artificial_intelligence_crime import settings
+from Service_Provider import views as serviceprovider
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+    url(r'^$', remoteuser.index, name="index"),
+    url(r'^login/$', remoteuser.login, name="login"),
+    url(r'^Register1/$', remoteuser.Register1, name="Register1"),
+    url(r'^Predict_Crime_Type/$', remoteuser.Predict_Crime_Type, name="Predict_Crime_Type"),
+    url(r'^ViewYourProfile/$', remoteuser.ViewYourProfile, name="ViewYourProfile"),
+    url(r'^serviceproviderlogin/$',serviceprovider.serviceproviderlogin, name="serviceproviderlogin"),
+    url(r'View_Remote_Users/$',serviceprovider.View_Remote_Users,name="View_Remote_Users"),
+    url(r'^charts/(?P<chart_type>\w+)', serviceprovider.charts,name="charts"),
+    url(r'^charts1/(?P<chart_type>\w+)', serviceprovider.charts1, name="charts1"),
+    url(r'^likeschart/(?P<like_chart>\w+)', serviceprovider.likeschart, name="likeschart"),
+    url(r'^View_Crime_Type_Ratio/$', serviceprovider.View_Crime_Type_Ratio, name="View_Crime_Type_Ratio"),
+    url(r'^train_model/$', serviceprovider.train_model, name="train_model"),
+    url(r'^View_Prediction_Of_Crime_Type/$', serviceprovider.View_Prediction_Of_Crime_Type, name="View_Prediction_Of_Crime_Type"),
+    url(r'^Download_Predicted_DataSets/$', serviceprovider.Download_Predicted_DataSets, name="Download_Predicted_DataSets"),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ views.py
@@ -0,0 +1,247 @@
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
+from django.db.models import Count
+from django.db.models import Q
+import datetime
+import xlwt
+from django.http import HttpResponse
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,prediction_Of_crime_type,detection_ratio,detection_accuracy
+
+
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
+        password = request.POST.get('password')
+        if admin == "Admin" and password =="Admin":
+            detection_accuracy.objects.all().delete()
+            return redirect('View_Remote_Users')
+
+    return render(request,'SProvider/serviceproviderlogin.html')
+
+def View_Crime_Type_Ratio(request):
+    detection_ratio.objects.all().delete()
+    ratio = ""
+    kword = 'Social Engineering'
+    print(kword)
+    obj = prediction_Of_crime_type.objects.all().filter(Q(Prediction=kword))
+    obj1 = prediction_Of_crime_type.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
+
+    ratio1 = ""
+    kword1 = 'Misinformation'
+    print(kword1)
+    obj1 = prediction_Of_crime_type.objects.all().filter(Q(Prediction=kword1))
+    obj11 = prediction_Of_crime_type.objects.all()
+    count1 = obj1.count();
+    count11 = obj11.count();
+    ratio1 = (count1 / count11) * 100
+    if ratio1 != 0:
+        detection_ratio.objects.create(names=kword1, ratio=ratio1)
+
+    ratio12 = ""
+    kword12 = 'Hacking'
+    print(kword12)
+    obj12 = prediction_Of_crime_type.objects.all().filter(Q(Prediction=kword12))
+    obj112 = prediction_Of_crime_type.objects.all()
+    count12 = obj12.count();
+    count112 = obj112.count();
+    ratio12 = (count12 / count112) * 100
+    if ratio12 != 0:
+        detection_ratio.objects.create(names=kword12, ratio=ratio12)
+
+    ratio123 = ""
+    kword123 = 'Autonomous weapon systems'
+    print(kword123)
+    obj123 = prediction_Of_crime_type.objects.all().filter(Q(Prediction=kword123))
+    obj1123 = prediction_Of_crime_type.objects.all()
+    count123 = obj123.count();
+    count1123 = obj1123.count();
+    ratio123 = (count123 / count1123) * 100
+    if ratio123 != 0:
+        detection_ratio.objects.create(names=kword123, ratio=ratio123)
+
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Crime_Type_Ratio.html', {'objs': obj})
+
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
+
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
+
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
+
+def View_Prediction_Of_Crime_Type(request):
+    obj =prediction_Of_crime_type.objects.all()
+    return render(request, 'SProvider/View_Prediction_Of_Crime_Type.html', {'list_objects': obj})
+
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
+
+
+def Download_Predicted_DataSets(request):
+
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Datasets.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = prediction_Of_crime_type.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+
+        row_num = row_num + 1
+
+        ws.write(row_num, 0, my_row.FID, font_style)
+        ws.write(row_num, 1, my_row.url, font_style)
+        ws.write(row_num, 2, my_row.length_url, font_style)
+        ws.write(row_num, 3, my_row.length_hostname, font_style)
+        ws.write(row_num, 4, my_row.Source_IP, font_style)
+        ws.write(row_num, 5, my_row.Source_Port, font_style)
+        ws.write(row_num, 6, my_row.Destination_IP, font_style)
+        ws.write(row_num, 7, my_row.Destination_Port, font_style)
+        ws.write(row_num, 8, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def train_model(request):
+    detection_accuracy.objects.all().delete()
+
+    df = pd.read_csv('Datasets.csv')
+
+    def apply_response(Label):
+        if (Label == 0):
+            return 0
+        elif (Label == 1):
+            return 1
+        elif (Label == 2):
+            return 2
+        elif (Label == 3):
+            return 3
+
+    df['results'] = df['Label'].apply(apply_response)
+
+    cv = CountVectorizer()
+    X = df['url']
+    y = df['results']
+
+    print("URl")
+    print(X)
+    print("Results")
+    print(y)
+
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+    print("Naive Bayes")
+
+    from sklearn.naive_bayes import MultinomialNB
+
+    NB = MultinomialNB()
+    NB.fit(X_train, y_train)
+    predict_nb = NB.predict(X_test)
+    naivebayes = accuracy_score(y_test, predict_nb) * 100
+    print("ACCURACY")
+    print(naivebayes)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_nb))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_nb))
+    models.append(('naive_bayes', NB))
+    detection_accuracy.objects.create(names="Naive Bayes", ratio=naivebayes)
+
+    print("Extra Tree Classifier")
+    from sklearn.tree import ExtraTreeClassifier
+    etc_clf = ExtraTreeClassifier()
+    etc_clf.fit(X_train, y_train)
+    etcpredict = etc_clf.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, etcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, etcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, etcpredict))
+    models.append(('RandomForestClassifier', etc_clf))
+    detection_accuracy.objects.create(names="Extra Tree Classifier", ratio=accuracy_score(y_test, etcpredict) * 100)
+
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    models.append(('svm', lin_clf))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+    print("Logistic Regression")
+
+    from sklearn.linear_model import LogisticRegression
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('logistic', reg))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+    print("Decision Tree Classifier")
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    dtcpredict = dtc.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, dtcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, dtcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, dtcpredict))
+    models.append(('DecisionTreeClassifier', dtc))
+    detection_accuracy.objects.create(names="Decision Tree Classifier", ratio=accuracy_score(y_test, dtcpredict) * 100)
+
+    csv_format = 'Results.csv'
+    df.to_csv(csv_format, index=False)
+    df.to_markdown
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/train_model.html', {'objs': obj})
